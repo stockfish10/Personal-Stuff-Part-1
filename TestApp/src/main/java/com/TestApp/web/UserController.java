df931157ffe322e30a5bea16118a7de3e6206d5b
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-02-03 15:09:52 +0200

    Add files via upload

--- TestApp/src/main/java/com/TestApp/web/UserController.java
+++ TestApp/src/main/java/com/TestApp/web/UserController.java
@@ -2,14 +2,12 @@ package com.TestApp.web;
 
 import com.TestApp.model.entities.UserEntity;
 import com.TestApp.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     private final UserRepository userRepository;
