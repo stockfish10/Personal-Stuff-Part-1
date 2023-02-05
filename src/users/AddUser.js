73be9d6e861a2021631c1ebd49a78f41b450b895
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-02-05 18:04:01 +0200

    TestApp front end

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function AddUser() {
+
+    let navigate = useNavigate()
 
     const [user, setUser] = useState({
         name:"",
@@ -14,12 +17,33 @@ export default function AddUser() {
         setUser({...user,[e.target.name]:e.target.value})
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        await fetch("http://localhost:8080/addUser",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({
+                    name,
+                    username,
+                    email
+                })
+            }).then(()=>{
+                alert("User added!");
+            });
+        
+        navigate("/");
+    }
+
   return (
     <div className="container">
         <div className="row">
             <div className="col-md-6 offset-md-3 border rounder p-4 mt-2 shadow">
                 <h2 className="text-center m-4">Register User</h2>
-
+            <form onSubmit={(e)=>onSubmit(e)}>
                 <div className="mb-3">
                     <label htmlFor="Name" className="form-label">
                         Name
@@ -56,8 +80,8 @@ export default function AddUser() {
                      onChange={(e)=>onInputChange(e)}/>
                 </div>
                 <button type="submit" className="btn btn-outline-primary">Submit</button>
-                <button type="submit" className="btn btn-outline-danger mx-2">Cancel</button>
-                
+                <Link className="btn btn-outline-danger mx-2" to="/">Cancel</Link>
+                </form>
             </div>
         </div>
     </div>
