471aa204c02f456ef110b04a76486d7db130c756
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-10-18 18:18:40 +0300

    Some tasks

--- Codewars/Kata/src/DoubleLinear.java
+++ Codewars/Kata/src/DoubleLinear.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 class DoubleLinear {
 
+    //Does not pass all tests on codewars due to "Max Buffer Size Reached (1.5 MiB)"
     public static int dblLinear (int n) {
-        // your code
+        List<Integer> numbers = new ArrayList<>(List.of(1));
+
+        int x = 0;
+        int y = 0;
+
+        while (numbers.size() <= n ) {
+
+            int nextX = 2 * numbers.get(x) + 1;
+            int nextY = 3 * numbers.get(y) + 1;
+
+            if (nextX <= nextY) {
+                numbers.add(nextX);
+                x++;
+
+                if (nextX == nextY) {
+                    y++;
+                }
+            } else {
+                numbers.add(nextY);
+                y++;
+            }
+        }
+        System.out.println(numbers);
+        return numbers.get(n);
     }
 }
