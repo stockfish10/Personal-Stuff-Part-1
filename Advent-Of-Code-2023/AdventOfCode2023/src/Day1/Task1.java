336380d43d8fa274df26e4fbdaffb3c7f84c2c94
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-12-05 15:56:35 +0200

    Day 1 task 1 completed

--- Advent-Of-Code-2023/AdventOfCode2023/src/Day1/Task1.java
+++ Advent-Of-Code-2023/AdventOfCode2023/src/Day1/Task1.java
@@ -1,4 +1,41 @@
 package Day1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File fp = new File("src/Day1/Input1.txt");
+        FileReader fr = new FileReader(fp);
+        BufferedReader br = new BufferedReader(fr);
+
+        List<String> list = br.lines().toList();
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String row : list) {
+            String numberOnly= row.replaceAll("[^0-9]", "");
+
+            if (numberOnly.length() == 1) {
+                numbers.add(Integer.parseInt(numberOnly + numberOnly));
+            } else if (numberOnly.length() == 2) {
+                numbers.add(Integer.parseInt(numberOnly));
+            } else {
+                String current = numberOnly.substring(0,1) + numberOnly.substring(numberOnly.length()-1);
+                numbers.add(Integer.parseInt(current));
+            }
+        }
+
+
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+
+        System.out.println(sum);
+    }
 }
