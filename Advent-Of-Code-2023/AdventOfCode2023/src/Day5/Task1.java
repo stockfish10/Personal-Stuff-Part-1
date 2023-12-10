5ce1ded561484a04679dc89aae5774a7df7bc51d
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-12-10 17:22:39 +0200

    Started day 6 task 1, skipped day 5 for during the weekdays.

---
+++ Advent-Of-Code-2023/AdventOfCode2023/src/Day5/Task1.java
@@ -0,0 +1,17 @@
+package Day5;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File fp = new File("src/Day4/Input.txt");
+        FileReader fr = new FileReader(fp);
+        BufferedReader br = new BufferedReader(fr);
+
+        List<String> list = br.lines().toList();
+    }
+}
