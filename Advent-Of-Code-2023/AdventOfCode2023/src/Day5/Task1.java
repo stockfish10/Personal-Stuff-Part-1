e1643767d3328b93962112d45d3679346bd36d59
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-12-11 15:41:15 +0200

    Finished task 1 day 6.

--- Advent-Of-Code-2023/AdventOfCode2023/src/Day5/Task1.java
+++ Advent-Of-Code-2023/AdventOfCode2023/src/Day5/Task1.java
@@ -1,17 +1,48 @@
 package Day5;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class Task1 {
-    public static void main(String[] args) throws FileNotFoundException {
-        File fp = new File("src/Day4/Input.txt");
+    public static void main(String[] args) throws IOException {
+        File fp = new File("src/Day5/Input.txt");
         FileReader fr = new FileReader(fp);
         BufferedReader br = new BufferedReader(fr);
 
-        List<String> list = br.lines().toList();
+        String timeMeasurements = br.readLine();
+        String distanceMeasurements = br.readLine();
+
+        int [] raceDurationValues = Arrays.stream(timeMeasurements.split(":")[1]
+                .trim()
+                .split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int [] raceDistanceValues = Arrays.stream(distanceMeasurements.split(":")[1]
+                        .trim()
+                        .split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+
+        int sum = 1;
+
+        for (int i = 0; i < raceDurationValues.length; i++) {
+
+            int currentRaceDuration = raceDurationValues[i];
+            int currentRaceDistance = raceDistanceValues[i];
+
+            int currentWaysToWin = 0;
+
+            for (int j = 1; j < currentRaceDuration; j++) {
+                int currentDistanceTraveled = j * (currentRaceDuration-j);
+
+                if (currentDistanceTraveled > currentRaceDistance) {
+                    currentWaysToWin++;
+                }
+            }
+            sum*=currentWaysToWin;
+        }
+
+        System.out.println(sum);
     }
 }
