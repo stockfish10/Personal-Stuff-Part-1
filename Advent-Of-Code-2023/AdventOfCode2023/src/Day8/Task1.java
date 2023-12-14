58e7b254b5ef93d5a2d378bed4a7e6a5c3104177
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-12-14 19:42:59 +0200

    Did Day 8 task 1, took ages to realize that we always start from AAA, not from the first element in the document.

--- Advent-Of-Code-2023/AdventOfCode2023/src/Day8/Task1.java
+++ Advent-Of-Code-2023/AdventOfCode2023/src/Day8/Task1.java
@@ -1,17 +1,54 @@
 package Day8;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Task1 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         File fp = new File("src/Day8/Input.txt");
         FileReader fr = new FileReader(fp);
         BufferedReader br = new BufferedReader(fr);
 
-        List<String> list = br.lines().toList();
+        char [] directions = br.readLine().toCharArray();
+
+        List<String> lines = br.lines().skip(1).toList();
+
+
+        Map<String, Node> nodes = lines.stream().map(Node::new)
+                .collect(Collectors.toMap(o->o.mainPart,o->o));
+
+        Node currentNode = nodes.get("AAA");
+
+        int steps = 0;
+
+        while (!"ZZZ".equals(currentNode.mainPart)) {
+            for (char direction : directions) {
+                currentNode = nodes.get(direction == 'L' ? currentNode.L : currentNode.R);
+                steps++;
+                if ("ZZZ".equals(currentNode.mainPart)){
+                    break;
+                }
+            }
+        }
+        System.out.println(steps);
     }
+        static class Node {
+            String mainPart;
+            String L;
+            String R;
+
+            Node(String line) {
+                mainPart = line.split(" = ")[0];
+                String otherPart = line.split(" = ")[1];
+
+                otherPart = otherPart.substring(1, otherPart.length() - 1).trim();
+
+                L = otherPart.split(", ")[0];
+                R = otherPart.split(", ")[1];
+            }
+        }
 }
